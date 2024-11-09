b5541494373df8926610be54d1344be9e76bbb3f
John Hayduk <jhayduk@cisco.com>
2024-11-09 18:26:03 -0500

    Add events and screen as inputs to the update method.

--- GameElement.py
+++ GameElement.py
@@ -6,9 +6,9 @@ so it has x and y positions (as well as the topleft, bottom, etc. variables
 that Rect objects have). It also has an image (instantiated with a filename),
 and a velocity in 2D space.
 
-The x and y position are always defined in pixels and refer to the top left
-corner of the image on the screen. This value is always relative to the top
-left corner of the screen which is defined to be at x=0, y=0.
+The x and y positions are always defined in pixels and refer to the top left
+corner of the image. The x and y values are always relative to the top left
+corner of the game screen itself, which is defined to be at x=0, y=0.
 
 The velocity is a Vector2 value in pixels per millisecond (ppm), with a unit
 vector pointing 1 ppm to the right and 1 ppm down (↘).
@@ -24,11 +24,10 @@ once per frame, and that update is called before draw each time.
 Both update and draw are normally overridden in subclasses of GameElement,
 but this class' version can be used for simple elements.
 """
-from typing import overload
-
 import pygame
+from pygame import Surface
+from pygame.event import Event
 from pygame.math import Vector2
-from typing import override
 
 
 class GameElement(pygame.Rect):
@@ -79,18 +78,37 @@ class GameElement(pygame.Rect):
         # Initialize the velocity
         self.velocity = velocity
 
-    def update(self, dt: int):
+    def update(self, dt: int, events: list[Event] = None, screen: Surface = None):
         """
         Update the position of the GameElement based on its velocity and the time delta.
 
+        Warning: If overridden, this method should be overridden completely.
+                    (i.e. do NOT use super().update())
+
         :param dt: The time delta in milliseconds since the last update.
+        :param events: A list of the pygame events detected since the last
+                        frame. Not all elements care about events, so this
+                        is optional and defaults None if omitted. When
+                        overriding this method, the fact that the events can
+                        be None MUST be taken into account if it is used.
+        :param screen: The surface on which this GameElement will ultimately be
+                        drawn. This can be used to determine if this element
+                        will still be on the screen when drawn. Not all
+                        elements care about the surface, so it is optional
+                        and defaults to None if omitted. When overriding
+                        this method, the fact that the screen can be None
+                        MUST be taken into account if it is used.
+
         """
         self.x += self.velocity.x * dt
         self.y += self.velocity.y * dt
 
-    def draw(self, screen: pygame.Surface):
+    def draw(self, screen: Surface):
         """
         Draw this object's image on the given screen surface at its current position.
+
+        Warning: If overridden, this method should be overridden completely.
+                    (i.e. do NOT use super().update())
 
         :param screen: The display surface this GameElement is to be drawn in.
             The element's position is always relative to the top-left corner
